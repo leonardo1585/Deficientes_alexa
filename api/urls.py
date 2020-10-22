e1f35e53a2384ae3f2ac8f0e69f3fad660f58c78
Leonardo <lleonardoamaral2011@hotmail.com>
2020-10-22 16:06:21 -0300

    parte3

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from core.views import PessoaViewSet
 from rest_framework import routers
 from django_ask_sdk.skill_adapter import SkillAdapter
-import projeto_deficientes
+import Deficientes_alexa
 
 
 router = routers.DefaultRouter()
 router.register(r'pessoas', PessoaViewSet)
-view = SkillAdapter.as_view(skill=projeto_deficientes.sb.create())
+view = SkillAdapter.as_view(skill=Deficientes_alexa.sb.create())
 # router.register(r'helloworld', LaunchRequestHandler)
 
 urlpatterns = [
